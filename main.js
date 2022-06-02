d67e2f05b4245ace9356bba95a5464e98d8f8885
Brillian Musyafa <brillianmusyafa@gmail.com>
2022-06-02 11:02:48 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var map = L.map('map').setView([-4.5094085922347, 104.51042890549], 9);
+var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+$.get('data.json', function (data) {
+    var mark = data['data'];
+    mark.forEach(function (dt) {
+        if (dt.hasOwnProperty('text')) {
+            var text = 'Link : <a target="_blank" href="http://' + dt.text + '">' + dt.text + '</a>';
+        } else {
+            var text = ''
+        }
+        var marker = L.marker([dt.lat, dt.lng]).addTo(map)
+            .bindPopup('Kecamatan : ' + dt.kecamatan + '</br>' + text);
+    });
+
+})
